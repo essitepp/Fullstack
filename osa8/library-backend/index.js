0bc98915b5447a52444f026c96eb20ec62b32c48
Essi Tepponen <43061528+essitepp@users.noreply.github.com>
2021-01-16 00:18:22 +0200

    8.8

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -105,6 +106,19 @@ const typeDefs = gql`
     allBooks(author:String, genre:String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
+  }
 `
 
 const resolvers = {
@@ -127,6 +141,29 @@ const resolvers = {
     bookCount: (root) => {
       const booksByAuthor = books.filter(b => b.author === root.name)
       return booksByAuthor.length
+    }
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      if (!authors.find(a => a.name === args.author)) {
+        const author = {
+          name: args.author,
+          id: uuid()
+        }
+        authors = authors.concat(author)
+      }
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      return book
+    },
+    editAuthor: (root, args) => {
+      author = authors.find(a => a.name === args.name)
+      if (!author) {
+        return null
+      }
+      author = { ...author, born: args.setBornTo }
+      authors = authors.map(a => a.name === author.name ? author : a)
+      return author
     }
   }
 }
