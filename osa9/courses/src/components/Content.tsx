916c05104930c100ea72b5b456397d94e7a552b9
Essi Tepponen <43061528+essitepp@users.noreply.github.com>
2021-08-25 17:37:02 +0300

    9.14

---
+++ osa9/courses/src/components/Content.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { CoursePart } from '../types';
+
+const Content = ({ courseParts }: { courseParts: CoursePart[] }) => {
+  return (
+    <div>
+      {courseParts.map(part =>  
+        <p key={part.name}> {part.name} {part.exerciseCount} </p>
+      )}
+    </div>
+  );
+};
+
+export default Content;
